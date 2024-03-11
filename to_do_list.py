c657ecc945e0442dec75a0b84801e6ec6b598597
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-11 08:36:16 -0400

    rewrote initial task class

--- to_do_list.py
+++ to_do_list.py
@@ -1,8 +1,13 @@
 class Task:
-    def __init__(self, task_id, description, completion_status = False):
-        self.task.id = task_id
+    id_counter = 1
+
+    def __init__(self, description):
+        self.id = Task.id_counter
+        Task.id_counter += 1
         self.description = description
-        self.completion_status = completion_status
+        self.completed = False
 
-
+    def display(self):
+        status = "Completed" if self.completed else "Not Completed"
+        print(f'Task ID: {self.id}, Description: {self.description}, Status: {status}')
 

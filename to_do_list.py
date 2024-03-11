41f3419287b861aa64df41752dba9411f7480eb1
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-11 08:26:14 -0400

    Initial commit

---
+++ to_do_list.py
@@ -0,0 +1,8 @@
+class Task:
+    def __init__(self, task_id, description, completion_status = False):
+        self.task.id = task_id
+        self.description = description
+        self.completion_status = completion_status
+
+
+
